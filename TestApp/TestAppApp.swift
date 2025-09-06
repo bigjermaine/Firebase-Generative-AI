cd4960ec7fec12d2c56ff96a062a2b36459f7f63
jermaine-iris <danieljermaine97@gmail.com>
2025-09-06 16:33:10 +0100

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Daniel Jermaine on 06/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
